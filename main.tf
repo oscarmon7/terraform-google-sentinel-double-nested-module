61e7e08350f30892566ad96858836529a02e11fb
Oscar Monleon <oscar.monleon@gft.com>
2021-03-05 17:18:06 +0000

    renamed final_module to simple_module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "attach_gke_service_project" {
-  source             = "modules/intermediate_module"
+  source             = "./modules/intermediate_module"
   environment        = var.environment
   organization       = var.organization
   service_project_id = var.service_project_id
