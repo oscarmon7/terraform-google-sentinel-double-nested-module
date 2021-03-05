1c3a0bcede3d81cf524f1b9b8025e2fe1f73d084
Oscar Monleon <oscar.monleon@gft.com>
2021-03-05 17:56:34 +0000

    fix relative path

--- modules/intermediate_module/main.tf
+++ modules/intermediate_module/main.tf
@@ -1,5 +1,5 @@
 module "attach_gke_service_project" {
-  source             = "./modules/simple_module"
+  source             = "../simple_module"
   environment        = var.environment
   organization       = var.organization
   service_project_id = var.service_project_id
