7da0d5cb0af38f3432a52f4ac5727e0042bb41c2
Oscar Monleon <oscar.monleon@gft.com>
2021-03-09 14:56:35 +0000

    Generate different host projects ids

--- modules/simple_module/locals.tf
+++ modules/simple_module/locals.tf
@@ -6,14 +6,14 @@ locals {
       "prd" : "host-project-2",
     },
     "ot" : {
-      "dev" : "host-project-0",
-      "uat" : "host-project-1",
-      "prd" : "host-project-2",
+      "dev" : "host-project-3",
+      "uat" : "host-project-4",
+      "prd" : "host-project-5",
     },
     "op" : {
-      "dev" : "host-project-0",
-      "uat" : "host-project-1",
-      "prd" : "host-project-2",
+      "dev" : "host-project-6",
+      "uat" : "host-project-7",
+      "prd" : "host-project-8",
     },
   }
 }
